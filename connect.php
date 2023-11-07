df5e74ffaf9c3f7d9fee5204a7a80c929a14f9dc
anasada <122490591+anasada@users.noreply.github.com>
2023-11-06 16:43:18 -0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,6 +3,7 @@
 // http://127.0.0.1/phpmyadmin
 
 $username=$_POST['username'];
+$trial_num=$_POST['trial_num'];
 $map_num=$_POST['map_num'];
 $versions=$_POST['version'];
 $rewards=$_POST['rewards'];
@@ -24,7 +25,7 @@ $con=mysqli_connect($dbhost,$dbuser,$dbpass,$db);
 
 if($con){
     // echo "connection successful!";
-    $sql="INSERT INTO `exp_responses`(username,map_num,versions,rewards,tokens,hammers,walls,point_to,conf_rating,serious,strategy,problems)VALUES('$username','$map_num','$versions','$rewards','$tokens','$hammers','$walls','$point_to','$conf_rating','$serious','$strategy','$problems')";
+    $sql="INSERT INTO `exp_responses`(username,trial_num,map_num,versions,rewards,tokens,hammers,walls,point_to,conf_rating,serious,strategy,problems)VALUES('$username', '$trial_num', '$map_num','$versions','$rewards','$tokens','$hammers','$walls','$point_to','$conf_rating','$serious','$strategy','$problems')";
     $result=mysqli_query($con,$sql);
 }else{
     die(mysqli_error($con));
